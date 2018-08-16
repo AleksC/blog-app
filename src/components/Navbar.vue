c8713cda44d7aa78bc36fa348c31c466f7838c31
AleksC <aleksandar.cosic1@yahoo.com>
2018-08-16 19:01:58 +0200

    1. zadatak

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <router-link class="nav-link" to='/posts'>Posts</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import AppPosts from './components/AppPosts'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const routes = [
+  { path: '/', redirect: '/posts' },
+  { path: '/posts', component: AppPosts }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
